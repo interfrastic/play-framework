2681976b667df5bcfba30401fb610ebd3da1e059
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2018-03-17 16:49:57 -0700

    Update tztest as combined test case

--- tztest/src/main/java/net/avax/cj/jdbc/tztest/Main.java
+++ tztest/src/main/java/net/avax/cj/jdbc/tztest/Main.java
@@ -3,116 +3,133 @@ package net.avax.cj.jdbc.tztest;
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Connection conn = null;
+    public static void main(String[] args) throws SQLException {
+        Boolean useLegacyDatetimeCode = false;
 
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/tztest"
-                    + "?user=release&password=release"
-                    + "&useSSL=false&serverTimezone=America/Los_Angeles");
+        String url = "jdbc:mysql://localhost/tztest?user=tztest"
+                + "&password=insecure&useSSL=false";
 
-            // Create Oracle DatabaseMetaData object
-            DatabaseMetaData meta = conn.getMetaData();
-
-            // gets driver info:
-            System.out.println("JDBC driver version is "
-                    + meta.getDriverVersion());
-
-            long now = System.currentTimeMillis();
-
-            PreparedStatement ps = null;
-            ResultSet rs = null;
-
-            try {
-                ps = conn.prepareStatement("INSERT INTO tztest"
-                                + " (test_date, test_datetime) VALUES (?, ?)",
-                        Statement.RETURN_GENERATED_KEYS);
-
-                ps.setDate(1, new Date(now));
-                ps.setTimestamp(2, new Timestamp(now));
-
-                int rowCount = ps.executeUpdate();
-
-                rs = ps.getGeneratedKeys();
-
-                if (rowCount == 1 && rs.next()) {
-                    int id  = rs.getInt(1);
-
-                    System.out.println("Inserted row with ID " +id);
-                } else {
-                    System.out.println("Something went wrong!");
-                }
-            } catch (SQLException ex) {
-                // handle any errors
-                System.out.println("SQLException: " + ex.getMessage());
-                System.out.println("SQLState: " + ex.getSQLState());
-                System.out.println("VendorError: " + ex.getErrorCode());
-            } finally {
-                // it is a good idea to release
-                // resources in a finally{} block
-                // in reverse-order of their creation
-                // if they are no-longer needed
-
-                if (rs != null) {
-                    try {
-                        rs.close();
-                    } catch (SQLException sqlEx) {
-                    } // ignore
-
-                    rs = null;
-                }
-
-                if (ps != null) {
-                    try {
-                        ps.close();
-                    } catch (SQLException sqlEx) {
-                    } // ignore
-
-                    ps = null;
-                }
-            }
-
-            Statement stmt = null;
-
-            try {
-                stmt = conn.createStatement();
-                rs = stmt.executeQuery("SELECT test_date FROM tztest");
-
-                // Now do something with the ResultSet ....
-            } catch (SQLException ex) {
-                // handle any errors
-                System.out.println("SQLException: " + ex.getMessage());
-                System.out.println("SQLState: " + ex.getSQLState());
-                System.out.println("VendorError: " + ex.getErrorCode());
-            } finally {
-                // it is a good idea to release
-                // resources in a finally{} block
-                // in reverse-order of their creation
-                // if they are no-longer needed
-
-                if (rs != null) {
-                    try {
-                        rs.close();
-                    } catch (SQLException sqlEx) {
-                    } // ignore
-
-                    rs = null;
-                }
-
-                if (stmt != null) {
-                    try {
-                        stmt.close();
-                    } catch (SQLException sqlEx) {
-                    } // ignore
-
-                    stmt = null;
-                }
-            }
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
+        if (useLegacyDatetimeCode != null) {
+            url += "&useLegacyDatetimeCode=" + useLegacyDatetimeCode;
         }
+
+        Connection conn = DriverManager.getConnection(url);
+
+        // Create Oracle DatabaseMetaData object
+        DatabaseMetaData meta = conn.getMetaData();
+
+        // gets driver info:
+        System.out.println("jdbcDriverVersion:                   "
+                + meta.getDriverVersion());
+        System.out.println("useLegacyDatetimeCode:               "
+                + useLegacyDatetimeCode);
+
+        // DROP TABLE IF EXISTS tztest;
+        // CREATE TABLE tztest (id INT(11) NOT NULL AUTO_INCREMENT, test_date
+        // DATE NOT NULL, test_datetime DATETIME(6) NOT NULL, test_timestamp
+        // TIMESTAMP DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (id));
+
+        long insertedTime = System.currentTimeMillis();
+
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO tztest"
+                        + " (test_date, test_datetime, test_timestamp) VALUES"
+                        + " (?, ?, NULL)",
+                Statement.RETURN_GENERATED_KEYS);
+
+        String insertedDateAsString = new Date(insertedTime).toString();
+        String insertedDatetimeAsString = new Timestamp(insertedTime)
+                .toString();
+
+        ps.setString(1, insertedDateAsString);
+        ps.setString(2, insertedDatetimeAsString);
+
+        int rowCount = ps.executeUpdate();
+
+        ResultSet rs = ps.getGeneratedKeys();
+
+        if (rowCount != 1 || !rs.next()) {
+            throw new AssertionError("Failed to insert row");
+        }
+
+        int insertedRowId = rs.getInt(1);
+
+        System.out.println();
+        System.out.println("insertedRowId:                       "
+                + insertedRowId);
+        System.out.println("insertedDateAsString:                "
+                + insertedDateAsString);
+        System.out.println("insertedDatetimeAsString:            "
+                + insertedDatetimeAsString);
+        System.out.println("insertedTimestamp:                   NULL");
+
+        ps = conn.prepareStatement("SELECT test_date, test_datetime,"
+                + " test_timestamp FROM tztest WHERE id = ?");
+
+        ps.setInt(1, insertedRowId);
+
+        if (!ps.execute() || !(rs = ps.getResultSet()).next()) {
+            throw new AssertionError("Failed to select inserted row");
+        }
+
+        String selectedInsertedDateAsString = rs.getString(1);
+        String selectedInsertedDatetimeAsString = rs.getString(2);
+        String selectedInsertedTimestampAsString = rs.getString(3);
+
+        System.out.println();
+        System.out.println("selectedInsertedDateAsString:        "
+                + selectedInsertedDateAsString);
+        System.out.println("selectedInsertedDatetimeAsString:    "
+                + selectedInsertedDatetimeAsString);
+        System.out.println("selectedInsertedTimestampAsString:   "
+                + selectedInsertedTimestampAsString);
+
+        long updatedTime = System.currentTimeMillis();
+
+        Date updatedDateAsDate = new Date(updatedTime);
+        Timestamp updatedDatetimeAsTimestamp = new Timestamp(updatedTime);
+        Timestamp updatedTimestampAsTimestamp = new Timestamp(updatedTime);
+
+        ps = conn.prepareStatement("UPDATE tztest SET test_date = ?,"
+                + " test_datetime = ?, test_timestamp = ? WHERE id = ?");
+
+        ps.setDate(1, updatedDateAsDate);
+        ps.setTimestamp(2, updatedDatetimeAsTimestamp);
+        ps.setTimestamp(3, updatedTimestampAsTimestamp);
+        ps.setInt(4, insertedRowId);
+
+        rowCount = ps.executeUpdate();
+
+        if (rowCount != 1) {
+            throw new AssertionError("Failed to update row");
+        }
+
+        System.out.println();
+        System.out.println("updatedDateAsDate:                   "
+                + updatedDateAsDate);
+        System.out.println("updatedDatetimeAsTimestamp:          "
+                + updatedDatetimeAsTimestamp);
+        System.out.println("updatedTimestampAsTimestamp:         "
+                + updatedTimestampAsTimestamp);
+
+        ps = conn.prepareStatement("SELECT test_date, test_datetime,"
+                + " test_timestamp FROM tztest WHERE id = ?");
+
+        ps.setInt(1, insertedRowId);
+
+        if (!ps.execute() || !(rs = ps.getResultSet()).next()) {
+            throw new AssertionError("Failed to select updated row");
+        }
+
+        Date selectedUpdatedDateAsDate = rs.getDate(1);
+        Timestamp selectedUpdatedDatetimeAsTimestamp = rs.getTimestamp(2);
+        Timestamp selectedUpdatedTimestampAsTimestamp = rs.getTimestamp(3);
+
+        System.out.println();
+        System.out.println("selectedUpdatedDateAsDate:           "
+                + selectedUpdatedDateAsDate);
+        System.out.println("selectedUpdatedDatetimeAsTimestamp:  "
+                + selectedUpdatedDatetimeAsTimestamp);
+        System.out.println("selectedUpdatedTimestampAsTimestamp: "
+                + selectedUpdatedTimestampAsTimestamp);
     }
 }
