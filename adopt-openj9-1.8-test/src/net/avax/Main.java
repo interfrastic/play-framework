b8340e4de54e0479225d157e911cd8934d2bc605
Michael Krueger <5394172+interfrastic@users.noreply.github.com>
2020-09-11 18:13:36 -0700

    Try adopt-openj9-1.8-test Apache URLEncodedUtils

--- adopt-openj9-1.8-test/src/net/avax/Main.java
+++ adopt-openj9-1.8-test/src/net/avax/Main.java
@@ -1,8 +1,15 @@
 package net.avax;
 
+import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.http.protocol.HTTP.DEF_CONTENT_CHARSET;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException {
@@ -11,5 +18,15 @@ public class Main {
         builder.addParameter("q", "C++ Programming");
 
         System.out.println("Here is your URI: " + builder);
+
+        List<NameValuePair> params = new ArrayList<>();
+
+        params.add(new BasicNameValuePair("username", "Homer Simpson"));
+        params.add(new BasicNameValuePair("password", "super+secret"));
+
+        String httpPostBody = URLEncodedUtils.format(params,
+                DEF_CONTENT_CHARSET);
+
+        System.out.println("Here is your HTTP POST body: " + httpPostBody);
     }
 }
